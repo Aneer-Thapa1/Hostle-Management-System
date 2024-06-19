561cdb5d1fecd73ca1bcd811aed355e912ef67c9
Aneer-Thapa1 <anir234thapa@gmail.com>
2024-06-19 20:16:12 +0545

    auth

--- Backend/config/dbConfig.js
+++ Backend/config/dbConfig.js
@@ -1,12 +1,18 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-
-    root: ,
-    host: ,
-    password: ,
-    database:
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "hostle_management",
 });
 
+db.connect((error) => {
+  if (error) {
+    console.log("Error connecting to database:", error);
+  } else {
+    console.log("Database connected successfully!!");
+  }
+});
 
-module.exports = db
+module.exports = db;
--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -1,1 +1,16 @@
 const db = require("../config/dbConfig");
+
+const signup = (req, res) => {
+  const [hostleName, hostleEmail, password] = req.body;
+
+  const getEmail = "SELECT hostleEmail FROM users;";
+
+  db.query(getEmail, (error, message) => {
+    if (error) {
+      console.log(error);
+    }
+
+    if (message.length > 0) {
+    }
+  });
+};
